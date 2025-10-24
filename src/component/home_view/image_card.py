555dda93d81bb66e1973a6f16c701aa703cae7cf
Kgnot <hricaurtem@proton.me>
2025-10-23 21:11:08 -0500

    Imagenes

--- src/component/home_view/image_card.py
+++ src/component/home_view/image_card.py
@@ -4,7 +4,7 @@ class ImageCard(ft.Container):
     def __init__(self, image_src: str, width: int = 350, height: int = 220):
         super().__init__(
             content=ft.Image(
-                src=image_src
+                src=image_src,
                 width=width,
                 height=height,
                 fit=ft.ImageFit.COVER
